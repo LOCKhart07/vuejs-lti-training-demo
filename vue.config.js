0d470adcc9589b4dd6a0ad92568b7ef6987d6652
Jenslee Dsouza <dsouzajenslee@gmail.com>
2023-03-06 16:33:47 +0530

    adding dist subtree

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === "production" ? "/cat-fiesta-website/" : "/",
+    publicPath: process.env.NODE_ENV === "production" ? "/vuejs-lti-training-demo/" : "/",
 };  
